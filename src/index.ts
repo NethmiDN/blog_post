73e1c21a8a2865e54794f000572a287419b1e886
Nethmi Diwyanga Nanayakkara <nethminanayakkara277@gmail.com>
2025-11-20 13:43:59 +0530

    feat: Update CORS configuration to allow requests from the production frontend

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ const app = express()
 app.use(express.json())
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://blog-post-fe-8wgl.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"] // optional
   })
 )
